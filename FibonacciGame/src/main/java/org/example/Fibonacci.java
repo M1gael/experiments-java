8b69bc2dc6fe94022d3a4b1ae187ab7867ba0e56
M1gael <smit.migael.johannes@gmail.com>
2024-08-08 03:45:57 +0200

    Issue was not the populate method , but a weird bug in the print. Added prints to help seek issue.

--- FibonacciGame/src/main/java/org/example/Fibonacci.java
+++ FibonacciGame/src/main/java/org/example/Fibonacci.java
@@ -4,37 +4,38 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Fibonacci {
-    private ArrayList<Integer> fibList = new ArrayList<Integer>();
+    protected ArrayList<Integer> fibList = new ArrayList<Integer>();
 
     public Fibonacci(){
-        fibList = createFibIndex();
-        //printList(fibList);
-        System.out.println(fibList.get(0));
-        System.out.println("weee from constructor");
+        createFibIndex();
+        printList();
+        //System.out.println("weee from constructor");
     }
 
-    private void printList(ArrayList fibList){
+    private void printList(){
+        System.out.println("Array length :" + fibList.size());
         for (int i = 0 ; i < fibList.size() ; i++){
-            System.out.println(fibList.get(i));
-            System.out.println("WEEEEEE");
+            System.out.println("Printing array index " + i + " as " +fibList.indexOf(i));
+            System.out.println("i just printed array index " + i);
+            //System.out.println("WEEEEEE from print method");
         }
     }
 
-    private ArrayList createFibIndex(){
-        ArrayList<Integer> newList = new ArrayList<Integer>();
+    private void createFibIndex(){
         int previous = 0 , current = 1 , next = 0 ;      // 1
-        for (int i = 1 ; i<100 ; i++){
+        for (int i = 0 ; i<100 ; i++){
             if (current <= 100){
-                fibList.add(current); // Add current to List if below 100
+                fibList.add(i , current); // Add current to List if below 100
+                System.out.println("Just added " + current + " to index " + i);
                 previous = current; // move current to previous
+                System.out.println("In loop " + i + " , Previous=" + previous + " Current=" + current);
                 current = current + previous;// get new current by sum of old current and previous current
+                System.out.println("New Current=" + current);
             }
             else{
                 break;
             }
-
         }
-        return newList;
     }
 }
 
