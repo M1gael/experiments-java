10ad8abbd18ed3b55f89e368d0166ab0fe001585
M1gael <smit.migael.johannes@gmail.com>
2024-08-13 03:35:28 +0200

    Started working on GUI , almost completed Machine.java , just have to check methods work as intended.

--- FibonacciGame/src/main/java/org/example/GUI.java
+++ FibonacciGame/src/main/java/org/example/GUI.java
@@ -4,13 +4,18 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
     JFrame frame = new JFrame();
-    //JLabel
+    JLabel machineOutLbl = new JLabel();
 
     public GUI(){
         frame.setLayout(null);
-        frame.setSize(500,500);
+        frame.setSize(800,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        machineOutLbl.setBounds(10 , 10 , 600 , 25);
+        machineOutLbl.setText("I will speak here");
+
+        frame.add(machineOutLbl);
     }
 
 
