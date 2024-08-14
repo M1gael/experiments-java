7d6a298282dffff0425a0bad5fd26b15f18a51a6
M1gael <smit.migael.johannes@gmail.com>
2024-08-14 02:41:48 +0200

    Small progress to GUI , still figuring out how to appropriately pass data between classes.

--- FibonacciGame/src/main/java/org/example/GUI.java
+++ FibonacciGame/src/main/java/org/example/GUI.java
@@ -1,21 +1,36 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame {
     JFrame frame = new JFrame();
     JLabel machineOutLbl = new JLabel();
+    JButton yesBtn = new JButton();
+    JButton noBtn = new JButton();
 
     public GUI(){
-        frame.setLayout(null);
-        frame.setSize(800,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+        frame.setLayout(null);
         frame.setVisible(true);
+
+        yesBtn.setText("True");
+        noBtn.setText("False");
+        yesBtn.setFocusable(false);
+        noBtn.setFocusable(false);
 
         machineOutLbl.setBounds(10 , 10 , 600 , 25);
         machineOutLbl.setText("I will speak here");
 
+        yesBtn.setBounds(200 , 400 , 80 , 40);
+        noBtn.setBounds(275 , 400 , 80 , 40);
+        yesBtn.setVisible(true);
+        noBtn.setVisible(true);
+
         frame.add(machineOutLbl);
+        frame.add(yesBtn);
+        frame.add(noBtn);
     }
 
 
--- FibonacciGame/src/main/java/org/example/Machine.java
+++ FibonacciGame/src/main/java/org/example/Machine.java
@@ -5,13 +5,13 @@ import java.util.Random;
 
 public class Machine {
     protected int guessNum = 0 ;
-    protected boolean fibTrueFalse = false , userAnswer = false;
     protected ArrayList<Integer> fibIndex =new ArrayList<Integer>();
 
     public Machine(){
         Random random = new Random(90);
         guessNum = (random.nextInt() + 10); //generate guess num
-        isFibonacci();
+        getFibIndex(); //get list from Fib class
+        isFibonacci(); //determine if the random number is part of the sequence
     }
 
     private void getFibIndex (){
@@ -19,19 +19,19 @@ public class Machine {
         fibIndex =  newList.getList();
     }
 
-    private void isFibonacci(){
+    private boolean isFibonacci(){
         if (fibIndex.contains(guessNum)){
-            fibTrueFalse = true;
+            return true;
         }else {
-            fibTrueFalse = false; //Ensure program works correctly if implementing loop
+            return false; //Ensure program works correctly if implementing loop
         }
     }
 
-    public void setBotResponse(){
-        if (fibTrueFalse == userAnswer){
-            System.out.println("Correct , the number " + guessNum + " is infact part of the fibonacci sequence." );
+    public String printAnswer(boolean userAnswer){
+        if (isFibonacci() == userAnswer){
+            return "Correct , the number " + guessNum + " is infact part of the fibonacci sequence.";
         }else{
-            System.out.println("Incorrect");
+            return  "Incorrect";
         }
     }
 
