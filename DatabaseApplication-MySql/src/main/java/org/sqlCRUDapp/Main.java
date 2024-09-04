332adcce926166915cb52ed327cde910f70821a7
M1gael <smit.migael.johannes@gmail.com>
2024-09-04 16:19:56 +0200

    Syncing main

--- DatabaseApplication-MySql/src/main/java/org/sqlCRUDapp/Main.java
+++ DatabaseApplication-MySql/src/main/java/org/sqlCRUDapp/Main.java
@@ -1,4 +1,20 @@
 package org.sqlCRUDapp;
 
+import java.sql.SQLException;
+
+
 public class Main {
+    public static void main(String[] args) {
+
+        StudentDAO studentDAO = new StudentDAO();
+
+        try {
+            // Create
+            // Read
+            // Update
+            // Delete
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
