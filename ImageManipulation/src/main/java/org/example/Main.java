8a3bde39440f70897f7270b967e0c1e1ce770cb8
Your Name <youremail@example.com>
2025-01-16 23:47:25 +0200

    Fully complete program . While error caused due to UI instance not closing fixed.

--- ImageManipulation/src/main/java/org/example/Main.java
+++ ImageManipulation/src/main/java/org/example/Main.java
@@ -5,27 +5,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner kb;
-    private static UI userInterface;
-    public static Picture myPicture; // Reserve objects to use in class methods
-
     public static void main(String[] args) throws IOException {
+        Scanner kb = new Scanner(System.in);
+        UI userInterface = new UI();
+        Picture myPicture = new Picture(); //instanced all objects
 
-        kb = new Scanner(System.in);
-        userInterface = new UI();
-        myPicture = new Picture(); //instanced all objects
+        String imgPath = "/home/debbie/gitdir/Java-Projects/ImageManipulation/src/main/java/org/example/image.png"; // Default value
 
-        String imgPath = "/home/debbie/gitdir/Java-Projects/ImageManipulation/src/main/java/org/example/image.png"; // A
-        //setPath(imgPath); // A , sets defaults at startup
-        userInterface.loadIcon(myPicture.setImage(imgPath)); // Invoke methods from both classes to buffer image and then store it
         do {
-            setPath(userInterface.chooseFile()); // Set path as what user chooses
-            System.out.print("Would you like to display another image ? y/n :"); // Loop
+            //User chooses a file for the next loop
+            imgPath = userInterface.chooseFile();
+            userInterface.loadIcon(myPicture.setImage(imgPath)); // Invoke methods from both classes to buffer image and then store it
 
-        } while (kb.nextLine().equalsIgnoreCase("y")); // broken loop
-    }
+            // User controls loop
+            System.out.println("Would you like to display another image ? y/n :");
 
-    private static void setPath(String imgPath) throws IOException{
-
+        } while (kb.nextLine().equalsIgnoreCase("y"));
+        kb.close();
+        userInterface.dispose();
+        System.exit(0); //properly end instances
     }
 }
--- ImageManipulation/src/main/java/org/example/UI.java
+++ ImageManipulation/src/main/java/org/example/UI.java
@@ -5,12 +5,11 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class UI extends JFrame{
-    //Instantiate objects that will have repeated use from class initiation
+
     private  FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
-    private JLabel displayLabel = new JLabel();
+    private JLabel displayLabel = new JLabel(); //Instantiate objects that will have repeated use from class initiation
 
     UI(){
-
         this.setSize(1280 , 720); // Stage frame
         this.setVisible(true);
         this.setResizable(false);
@@ -22,15 +21,15 @@ public class UI extends JFrame{
         displayLabel.setVerticalAlignment(JLabel.CENTER);
 
         this.add(displayLabel); // add label
-
     }
 
     public String chooseFile(){
         dialog.setMode(FileDialog.LOAD);
         dialog.setVisible(true);
-        String file = dialog.getFile();
+        String fileName = dialog.getFile();
+        String fileDir = dialog.getDirectory();
         dialog.dispose();
-        return file + " chosen.";
+        return fileDir + fileName ; // Have it return the dir and filename concat to be used
     }
 
     public void loadIcon(BufferedImage image){
