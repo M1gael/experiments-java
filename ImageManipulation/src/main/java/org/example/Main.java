5ac756500a48b708911087812353d500c1b895d6
Your Name <youremail@example.com>
2025-01-19 02:13:29 +0200

    Added buttons in exchange for terminal loop

--- ImageManipulation/src/main/java/org/example/Main.java
+++ ImageManipulation/src/main/java/org/example/Main.java
@@ -1,28 +1,12 @@
 package org.example;
 
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner kb = new Scanner(System.in);
         UI userInterface = new UI();
-        Picture myPicture = new Picture(); //instanced all objects
-
-        String imgPath = "/home/debbie/gitdir/Java-Projects/ImageManipulation/src/main/java/org/example/image.png"; // Default value
-
-        do {
-            //User chooses a file for the next loop
-            imgPath = userInterface.chooseFile();
-            userInterface.loadIcon(myPicture.setImage(imgPath)); // Invoke methods from both classes to buffer image and then store it
-
-            // User controls loop
-            System.out.println("Would you like to display another image ? y/n :");
-
-        } while (kb.nextLine().equalsIgnoreCase("y"));
-        kb.close();
-        userInterface.dispose();
-        System.exit(0); //properly end instances
     }
 }
--- ImageManipulation/src/main/java/org/example/UI.java
+++ ImageManipulation/src/main/java/org/example/UI.java
@@ -2,26 +2,45 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-public class UI extends JFrame{
+public class UI extends JFrame implements ActionListener {
 
+    private String imgPath = null;
     private  FileDialog dialog ;
+    private Picture myPicture;
     private JLabel displayLabel ; //Reserve objects that will have repeated use from class initiation
+    private JButton btnChooseImage , btnClose;
 
     UI(){
+        myPicture = new Picture();
         this.setSize(1920 , 1080); // Stage frame
-        this.setVisible(true);
         this.setResizable(true);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setLayout(null);
 
         displayLabel = new JLabel();
         //displayLabel.setText("Load Picture"); // Stage label defaults
-        displayLabel.setSize(1920 , 1080);
+        displayLabel.setBounds( 0 ,  0 , 1600 , 1080);
         displayLabel.setHorizontalAlignment(JLabel.CENTER);
         displayLabel.setVerticalAlignment(JLabel.CENTER);
+        displayLabel.setForeground(Color.green);
+        // Choose Image
+        btnChooseImage = new JButton("Choose Image");
+        btnChooseImage.setBounds(1650, 50, 150, 50);
+        btnChooseImage.addActionListener(this); // Attach listener
+        // Close
+        btnClose = new JButton("Close Application");
+        btnClose.setBounds(1650, 120, 150, 50);
+        btnClose.addActionListener(this); // Attach listener
 
         this.add(displayLabel); // add label
+        this.add(btnChooseImage);
+        this.add(btnClose);
+        this.setVisible(true);
     }
 
     public String chooseFile(){
@@ -37,5 +56,14 @@ public class UI extends JFrame{
     public void loadIcon(BufferedImage image){
         ImageIcon imageIcon = new ImageIcon(image);
         displayLabel.setIcon(imageIcon);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnChooseImage){
+            try{loadIcon(myPicture.setImage(chooseFile()));}
+            catch (IOException j){j.printStackTrace();}
+        }
+        if (e.getSource() == btnClose){System.exit(0);}
     }
 }
