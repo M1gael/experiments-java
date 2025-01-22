cf2e9e445e73433bed92cf051d699ad38cb6ce7c
Your Name <smit.migael.johannes@gmail.com>
2025-01-23 01:37:50 +0200

    Sync

---
+++ spring-photo-webapp/src/test/java/com/example/spring_photo_webapp/SpringPhotoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_photo_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPhotoWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
