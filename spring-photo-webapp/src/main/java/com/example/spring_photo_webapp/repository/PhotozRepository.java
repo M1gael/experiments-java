9e56cbdaf0c11bc594be21b46306973b905a6743
M1gael <smit.migael.johannes@gmail.com>
2025-02-07 18:39:27 +0200

    Removed old spring photos app.

--- spring-photo-webapp/src/main/java/com/example/spring_photo_webapp/repository/PhotozRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_photo_webapp.repository;
-
-import com.example.spring_photo_webapp.model.Photo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PhotozRepository extends CrudRepository<Photo, Integer>{ // predefined sql statements
-}
